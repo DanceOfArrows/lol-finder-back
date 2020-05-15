8a85f9b0ce4bf5fab993e99432eebe0258f7b32e
Seamus Le <seamust.le@gmail.com>
2020-05-15 13:11:33 -0700

    changed cors to accept all

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
+app.use(cors());
 app.use(session({
   resave: false,
   saveUninitialized: false,
