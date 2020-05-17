b23634f47d47f9f8e467fcdaae3a2b9c2a00bf08
Seamus Le <seamust.le@gmail.com>
2020-05-17 04:02:28 -0700

    removed old session code

--- app.js
+++ app.js
@@ -20,11 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: "this is a secret yes."
-}));
 
 app.use('/rotation', champRotation); // Free champ rotation
 app.use('/region', regionControl); // Change region to make requests to
